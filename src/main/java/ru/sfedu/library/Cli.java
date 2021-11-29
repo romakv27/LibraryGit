902631a1e033d591101d02e86bf15c70c7583958
romakv27 <kvarchiyarom@gmail.com>
2021-11-29 08:27:42 +0300

    add cli develop

---
+++ src/main/java/ru/sfedu/library/Cli.java
@@ -0,0 +1,12 @@
+package ru.sfedu.library;
+
+import java.io.IOException;
+
+import static ru.sfedu.library.Constants.SOUR;
+import static ru.sfedu.library.utils.ConfigurationUtil.getConfigurationEntry;
+
+public class Cli {
+    public static void main(String []args) throws IOException {
+    System.out.println("hello develop");
+    }
+}
